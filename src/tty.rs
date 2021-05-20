8ed5077ec3cbf6b2d9ba4d9c9d50bdc6b5708136
Jan Katzer <jan@katzer.dev>
2021-05-20 14:44:45 +0200

    Add tty self test and fix configuration (+ enable receive irq)

--- src/tty.rs
+++ src/tty.rs
@@ -1,19 +1,28 @@
 use crate::arch::x86_64::port::*;
 
+// https://wiki.osdev.org/Serial_Ports#Programming_the_Serial_Communications_Port
+// https://en.wikibooks.org/wiki/Serial_Programming/8250_UART_Programming
+
 /// COM 1
 const PORT: u16 = 0x3F8;
 
 pub unsafe fn enable_serial_tty() {
 	outb(PORT + 1, 0x00); // Disable all interrupts
 	outb(PORT + 3, 0x80); // Enable DLAB (set baud rate divisor)
-	outb(PORT + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
+	outb(PORT + 0, 0x03); // Set divisor to 3 (lo byte): 115200 / 3 = 38400 baud
 	outb(PORT + 1, 0x00); //                  (hi byte)
-	outb(PORT + 3, 0x03); // Disable DLAB, 8 bits, no parity, one stop bit
+	outb(PORT + 3, 0b00_000_0_11); // Disable DLAB, 8 bits, no parity, one stop bit
 	outb(PORT + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
-//	outb(port + 4, 0x0B); // IRQs enabled, RTS/DSR set
-	outb(PORT + 4, 0x00); // DEBUG: All IRQs disabled
-//	outb(port + 4, 0x1E); // Set in loopback mode, test the serial chip
-//	outb(port + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
+	outb(PORT + 4, 0x0B); // IRQs enabled, RTS/DSR set
+	outb(PORT + 4, 0x1E); // Set in loopback mode, test the serial chip
+	
+	outb(PORT + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
+	let read_byte = inb(PORT + 0);
+	if read_byte != 0xAE {
+		panic!("Serial chip quick test failed: got byte {}, expected {}", read_byte, 0xAE);
+	}
+	
+	outb(PORT + 1, 0x01); // Reenable interrupts
 	outb(PORT + 4, 0x0F); // Enter normal operating mode
 }
 
