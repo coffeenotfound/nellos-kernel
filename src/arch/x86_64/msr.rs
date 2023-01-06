4d4d7a46247ba440ab55028b00af773b6bb1662d
Jan Katzer <jan@katzer.dev>
2023-01-06 16:51:03 +0100

    Improve Msr by adding Copy, Clone derives and mutate()/mutate_raw()

--- src/arch/x86_64/msr.rs
+++ src/arch/x86_64/msr.rs
@@ -9,6 +9,7 @@ pub const SFMASK: Msr = Msr::from_nr(0xC000_0084);
 
 pub const IA32_APIC_BASE: Msr = Msr::from_nr(0x0000_001b); // TODO: Is this the right addr?
 
+#[derive(Copy, Clone)]
 pub struct Msr<T: MsrData = u64>(u32, PhantomData<*const T>);
 
 impl<T: MsrData> Msr<T> {
@@ -33,6 +34,13 @@ impl<T: MsrData> Msr<T> {
 	}
 	
 	#[inline(always)]
+	pub unsafe fn mutate(&self, op: impl FnOnce(T) -> T) {
+		let old_val = T::from_raw(read_msr(self.0));
+		let new_val = (op)(old_val).to_raw();
+		write_msr(self.0, new_val);
+	}
+	
+	#[inline(always)]
 	pub unsafe fn read_raw(&self) -> u64 {
 		read_msr(self.0)
 	}
@@ -40,6 +48,13 @@ impl<T: MsrData> Msr<T> {
 	#[inline(always)]
 	pub unsafe fn write_raw(&self, val: u64) {
 		write_msr(self.0, val);
+	}
+	
+	#[inline(always)]
+	pub unsafe fn mutate_raw(&self, op: impl FnOnce(u64) -> u64) {
+		let old_val = read_msr(self.0);
+		let new_val = (op)(old_val);
+		write_msr(self.0, new_val);
 	}
 }
 
